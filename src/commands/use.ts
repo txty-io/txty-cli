a65bf61089e4571b49af0c1b055eff226df25df4
Gottfried Chen <gottfried.chen@allaboutapps.at>
2021-12-18 11:36:33 +0100

    add comments

--- src/commands/use.ts
+++ src/commands/use.ts
@@ -5,6 +5,9 @@ import * as path from "path";
 import { Logger } from "../Logger";
 import { Validators } from "../Validators";
 
+// nvm like support for multiple texterify servers by allowing you to switch between
+// multiple global configs named e.g. ~/.texterify.json.serverA and ~/.texterify.json.serverB
+// by calling "texterify use serverA" or "texterify use serverB".
 export default class Use extends Command {
     static description =
         "Switch to a different texterify server (e.g. if different projects use different servers). " +
@@ -21,10 +24,12 @@ export default class Use extends Command {
     async run() {
         const { args } = this.parse(Use);
 
+        // Create total path to new global config
         const configName = path.join(os.homedir(), ".texterify.json." + args.global_config_name);
 
         Validators.ensureFileExists(configName, "Global config not found:");
 
+        // Copy to current global config
         fs.copyFile(configName, path.join(os.homedir(), ".texterify.json"), (err) => {
             if (err) {
                 Logger.error("Failed to copy config file.");
