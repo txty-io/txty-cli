69b7b6bf56371d3d67c85a6af3b96532280a388d
Christoph Werner <christoph@chrztoph.com>
2022-01-12 15:37:35 +0100

    Disable login command for now

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -10,50 +10,50 @@ import * as nconf from "nconf";
 import * as path from "path";
 import { cli } from "cli-ux";
 
-export default class Add extends Command {
-    static description = "log in to a Texterify server";
+// export default class Add extends Command {
+//     static description = "log in to a Texterify server";
 
-    static flags = {
-        help: flags.help({ char: "h" })
-    };
+//     static flags = {
+//         help: flags.help({ char: "h" })
+//     };
 
-    static examples = ["$ texterify login"];
+//     static examples = ["$ texterify login"];
 
-    async run() {
-        const { args, flags } = this.parse(Add);
+//     async run() {
+//         const { args, flags } = this.parse(Add);
 
-        const server = await cli.prompt("Which Texterify instance are you using?", {
-            default: "https://app.texterify.com"
-        });
+//         const server = await cli.prompt("Which Texterify instance are you using?", {
+//             default: "https://app.texterify.com"
+//         });
 
-        const email = await cli.prompt("What is your email address?", { required: true });
+//         const email = await cli.prompt("What is your email address?", { required: true });
 
-        const accessToken = await cli.prompt("Enter your access token?", {
-            required: true,
-            type: "hide"
-        });
+//         const accessToken = await cli.prompt("Enter your access token?", {
+//             required: true,
+//             type: "hide"
+//         });
 
-        // let response;
-        // try {
-        //     response = await KeysAPI.createKey({
-        //         projectId: projectId,
-        //         name: args.name,
-        //         description: flags.description || "",
-        //         defaultLanguageTranslation: args.content
-        //     });
-        // } catch (error) {
-        //     Logger.error("Failed to add key.");
-        //     showErrorFixSuggestions(error);
-        //     Validators.exitWithError();
-        // }
+//         // let response;
+//         // try {
+//         //     response = await KeysAPI.createKey({
+//         //         projectId: projectId,
+//         //         name: args.name,
+//         //         description: flags.description || "",
+//         //         defaultLanguageTranslation: args.content
+//         //     });
+//         // } catch (error) {
+//         //     Logger.error("Failed to add key.");
+//         //     showErrorFixSuggestions(error);
+//         //     Validators.exitWithError();
+//         // }
 
-        // globalConfig.setKey
+//         // globalConfig.setKey
 
-        // if (response?.errors) {
-        //     ErrorUtils.getAndPrintErrors(response.errors);
-        //     Validators.exitWithError();
-        // } else {
-        //     Logger.success(`Successfully added key "${args.name}".`);
-        // }
-    }
-}
+//         // if (response?.errors) {
+//         //     ErrorUtils.getAndPrintErrors(response.errors);
+//         //     Validators.exitWithError();
+//         // } else {
+//         //     Logger.success(`Successfully added key "${args.name}".`);
+//         // }
+//     }
+// }
