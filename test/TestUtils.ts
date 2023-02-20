1eb47c1711d1d41841e7ae3433fb3cf1dc6e3a9d
Christoph Werner <christoph@chrztoph.com>
2023-02-20 16:04:16 +0100

    Improve error logging for cypress commands

--- test/TestUtils.ts
+++ test/TestUtils.ts
@@ -1,25 +1,43 @@
-import fetch from "node-fetch";
+import fetch, { Response } from "node-fetch";
 
 export async function loadCLISeeds() {
+    let response: Response | null = null;
     try {
-        await fetch("http://localhost:3000/__cypress__/command", {
+        response = await fetch("http://localhost:3000/__cypress__/command", {
             method: "post",
-            body: JSON.stringify({ name: "load_seed_cli", options: {} })
+            body: JSON.stringify({ name: "load_seed_cldi", options: {} })
         });
-        console.log("Successfully loaded 'load_seed_cli'.");
     } catch (error) {
-        console.error("Failed to load CLI test seeds.");
+        console.error("Failed to load CLI test seeds");
+    }
+
+    if (response !== null) {
+        const jsonResponse = JSON.stringify(await response.json());
+        if (response.status !== 201) {
+            throw new Error(`Failed to execute cypress command: ${jsonResponse}`);
+        } else {
+            console.log("Successfully loaded 'load_seed_cli'", { code: response.status, response: jsonResponse });
+        }
     }
 }
 
 export async function cleanDatabase() {
+    let response: Response | null = null;
     try {
-        await fetch("http://localhost:3000/__cypress__/command", {
+        response = await fetch("http://localhost:3000/__cypress__/command", {
             method: "post",
             body: JSON.stringify({ name: "clean", options: {} })
         });
-        console.log("Successfully loaded 'clean'.");
     } catch (error) {
-        console.error("Failed to clean database for test run.");
+        console.error("Failed to clean database for test run");
+    }
+
+    if (response !== null) {
+        const jsonResponse = JSON.stringify(await response.json());
+        if (response.status !== 201) {
+            throw new Error(`Failed to execute cypress command: ${jsonResponse}`);
+        } else {
+            console.log("Successfully loaded 'clean'", { code: response.status, response: jsonResponse });
+        }
     }
 }
