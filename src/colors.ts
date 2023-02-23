db736d9939a6e57bfcc2548b3e8b4d1ea8d497b6
Christoph Werner <christoph@chrztoph.com>
2023-02-23 12:02:04 +0100

    Update chalk

--- src/colors.ts
+++ src/colors.ts
@@ -2,5 +2,5 @@ import chalk from "chalk";
 
 export const infoColor = chalk.bold.black;
 export const successColor = chalk.bold.green;
-export const warningColor = chalk.bold.keyword("orange");
+export const warningColor = chalk.bold.yellow;
 export const errorColor = chalk.bold.red;
